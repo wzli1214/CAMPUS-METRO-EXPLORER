bdcde896d29729dcbb014653d671657dc1f4d5ff
Weizhao Li <weizhaoli@Weizhaos-MacBook-Pro.local>
2019-02-23 12:01:08 -0500

    SharePreferences

--- app/src/main/java/com/example/weizhaoli/gwu_explorer/MainActivity.kt
+++ app/src/main/java/com/example/weizhaoli/gwu_explorer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weizhaoli.gwu_explorer
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -7,12 +8,16 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+
     private lateinit var destination: EditText
     private lateinit var go: Button
     private lateinit var alert: Button
+    private lateinit var remember: CheckBox
 
     private val textWatcher: TextWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {}
@@ -37,23 +42,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivity", "onCreate called")
+
+        AlertDialog.Builder(this)
+            .setTitle("Welcome!")
+            .setMessage("Please enter a location to travel to from GWU and use the alerts button to view Metro outages. \n\n" +
+                    "Note: current only works for locations which don't require a transfer."
+            ) .setPositiveButton("OK") { dialog, which ->
+                // User pressed OK
+            }
+            .show()
+
         destination = findViewById(R.id.destination)
         go = findViewById(R.id.go)
         alert = findViewById(R.id.alert)
+        remember = findViewById(R.id.remember)
 
 
         destination.addTextChangedListener(textWatcher)
 
 
-        AlertDialog.Builder(this)
-            .setTitle("Welcome!")
-            .setMessage("Please enter a location to travel to from GWU and use the alerts button to view Metro outages. \n\n" +
-                     "Note: current only works for locations which don't require a transfer."
-                    ) .setPositiveButton("OK") { dialog, which ->
-                // User pressed OK
-            }
-            .show()
-    }
+
+        if(remember.isChecked) {
+            val desSave = destination.text.toString()
+            val editor = getSharedPreferences("gwu_destination", Context.MODE_PRIVATE).edit()
+            editor.putString("destinationSave", desSave)
+            editor.apply()
+        }else {
+            val et = getSharedPreferences("gwu_destination", Context.MODE_PRIVATE).edit()
+            et.clear()
+            et.apply()
+        }
+
+//      Get the save destinationAddress
+//        val sp = getSharedPreferences("gwu_destination", 0)
+//        destination.setText(sp.getString("destinationSave", ""))
+
+        restoreInfo()
+
+        }
 
 
     override fun onStart() {
@@ -79,5 +105,11 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d("MainActivity", "onDestroy called")
+    }
+
+    fun restoreInfo() {
+        val sp = getSharedPreferences("gwu_destination", 0)
+        destination.setText(sp.getString("destinationSave", ""))
+
     }
 }
